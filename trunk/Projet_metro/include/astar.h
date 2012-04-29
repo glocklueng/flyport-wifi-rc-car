a9d2a2ca49594948dd868870635147692c257040
jeremy.fanguede <jeremy.fanguede@5bd66f22-8b68-2e66-7e8a-2c976194fa36>
2012-04-29 11:24:29 +0000

    astar

--- trunk/Projet_metro/include/astar.h
+++ trunk/Projet_metro/include/astar.h
@@ -2,15 +2,16 @@
 #define _ASTAR
 
 #include <stdlib.h>
-#include "include/donnees.h"
+#include "donnees.h"
 
+/* amélioration : faire une liste triée par c*/
 struct Data
 {
 	int num;
 	int a;
 	int h;
 	int c;
-	struct Data * suiv;
+	struct Data * next;
 };
 typedef struct Data Data;
 typedef struct Data * ListeData;
@@ -19,17 +20,26 @@ struct Souvenir
 {
 	int num;
 	int numPere;
-	//
-	struct Souvenir * suiv;
+	//truc en plus à voir
+	struct Souvenir * next;
 };
 typedef struct Souvenir * ListeSouv;
 
 struct Res
 {
 	int num;
-	//
-	struct Res * suiv;
+	//truc en plus à voir
+	struct Res * next;
 };
 typedef struct Res * ListeRes;
+
+ListeRes aStar(int numDep, int numArr, Station * plan);
+ListeRes reconstruire(ListeSouv souv, int numDep, int numArr);
+int heuristic(int numDep,int numArr,Station * plan);
+ListeRes ajouterRes( ListeRes l, int num);
+ListeSouv ajouterSouv( ListeSouv l, int numFils, int numPere);
+ListeData setData(ListeData l, int num , int a , int h);
+int isInListe(ListeData l,int num);
+int getCscore(ListeData l,int num);
 
 #endif
